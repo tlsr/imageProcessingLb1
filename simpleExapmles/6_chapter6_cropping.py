88a16a64c75e347d6677da59ceb39eb6f86c51f8
Bohdan Polishchuk <bohdan.polishchuk@nixsolutions.com>
2023-11-25 14:15:01 +0200

    rename title

--- simpleExapmles/6_chapter6_cropping.py
+++ simpleExapmles/6_chapter6_cropping.py
@@ -1,5 +1,4 @@
 import numpy as np
-import argparse
 import cv2
 import imutils
 
@@ -8,5 +7,5 @@ image = imutils.loadImage()
 cv2.imshow("Original", image)
 
 cropped = image[184:275 , 98:204]
-cv2.imshow("T-Rex Face", cropped)
+cv2.imshow("cat Face", cropped)
 cv2.waitKey(0)
